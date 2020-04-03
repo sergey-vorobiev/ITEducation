9761d6e869c1042c3df6c451179755b5f1178a58
Sergey <54852578+LittleTozz@users.noreply.github.com>
2020-04-03 14:37:48 +0300

    Kanban Board
    v1

---
+++ react/src/Card.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import CheckList from './CheckList';
+
+class Card extends Component {
+    constructor() {
+        super(...arguments);
+        this.state = {
+            showDetails: false
+        };
+    }
+
+    toggleDetails(){
+        this.setState({showDetails: !this.state.showDetails});
+    }
+
+    render() {
+        let cardDetails;
+        if (this.state.showDetails) {
+            cardDetails = (
+                <div className="card__details">
+                    {this.props.description}
+                    <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+                </div>
+            );
+        }
+        return (
+            <div className="card" onClick={this.toggleDetails.bind(this)}>
+                <div className="card__title">{this.props.title}</div>
+                {cardDetails}
+            </div>
+        );
+    }
+}
+
+export default Card;
